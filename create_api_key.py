4fa81ccce69e7e68c0c9faf27a09ac5102ca12c4
ramazan_kashipov <ramazan.kashipov@systeme.ru>
2023-12-21 02:48:41 +0300

    Add dialog flow api with intent detection

---
+++ create_api_key.py
@@ -0,0 +1,48 @@
+from google.cloud import api_keys_v2
+from google.cloud.api_keys_v2 import Key
+from environs import Env
+
+def create_api_key(project_id: str, suffix: str) -> Key:
+    """
+    Creates and restrict an API key. Add the suffix for uniqueness.
+
+    TODO(Developer):
+    1. Before running this sample,
+      set up ADC as described in https://cloud.google.com/docs/authentication/external/set-up-adc
+    2. Make sure you have the necessary permission to create API keys.
+
+    Args:
+        project_id: Google Cloud project id.
+
+    Returns:
+        response: Returns the created API Key.
+    """
+    # Create the API Keys client.
+    client = api_keys_v2.ApiKeysClient()
+
+    key = api_keys_v2.Key()
+    key.display_name = f"My first API key - {suffix}"
+
+    # Initialize request and set arguments.
+    request = api_keys_v2.CreateKeyRequest()
+    request.parent = f"projects/{project_id}/locations/global"
+    request.key = key
+
+    # Make the request and wait for the operation to complete.
+    response = client.create_key(request=request).result()
+
+    print(f"Successfully created an API key: {response.name}")
+    # For authenticating with the API key, use the value in "response.key_string".
+    # To restrict the usage of this API key, use the value in "response.name".
+    return response
+
+
+def main():
+    env = Env()
+    env.read_env()
+    project_id = env.str('GOOGLE_CLOUD_PROJECT')
+    with open('api_key.txt', 'w', encoding='utf-8') as file:
+        file.write(str(create_api_key(project_id, 'my')))
+
+if __name__ == "__main__":
+    main()
--- main.py
+++ main.py
@@ -1,16 +1,50 @@
 import logging
+import os
+import functools
 
 from environs import Env
 from telegram import Update
 from telegram.ext import Updater, CallbackContext, CommandHandler, MessageHandler, Filters
+from google.cloud import dialogflow
+
+
+def detect_intent_texts(project_id, session_id, texts, language_code):
+    """Returns the result of detect intent with tests as inputs.
+    Using the same `session_id` between requests allows continuation
+    of the conversation."""
+
+    session_client = dialogflow.SessionsClient()
+
+    session = session_client.session_path(project_id, session_id)
+    print(f'Session path: {session}\n')
+
+    text_input = dialogflow.TextInput(text=texts, language_code=language_code)
+    query_input = dialogflow.QueryInput(text=text_input)
+
+    response = session_client.detect_intent(
+        request={
+            "session": session,
+            "query_input": query_input,
+        }
+    )
+    print(f'''Query text: {response.query_result.query_text}
+    Detect intent: {response.query_result.intent.display_name} (confidence: {response.query_result.intent_detection_confidence}
+    Fulfillment text: {response.query_result.fulfillment_text}
+    ''')
+    return response.query_result.fulfillment_text
 
 
 def start(update: Update, context: CallbackContext):
     context.bot.send_message(chat_id=update.effective_chat.id, text='Здравствуйте!')
 
 
-def echo(update: Update, context: CallbackContext):
+def echo(update: Update, context: CallbackContext, project_id: str = '', language_code: str = ''):
     context.bot.send_message(chat_id=update.effective_chat.id, text=update.message.text)
+
+
+def answer(update: Update, context: CallbackContext, project_id: str = '', language_code: str = ''):
+    answer = detect_intent_texts(project_id, update.effective_chat.id, update.message.text, language_code)
+    context.bot.send_message(chat_id=update.effective_chat.id, text=answer)
 
 
 def main():
@@ -19,15 +53,25 @@ def main():
     env = Env()
     env.read_env()
     tg_bot_token = env.str('TG_BOT_TOKEN')
+    google_application_credentials_path = env.path('GOOGLE_APPLICATION_CREDENTIALS')
+    project_id = env.str('GOOGLE_CLOUD_PROJECT')
+    language_code = env.str('LANGUAGE_CODE')
 
+    print(google_application_credentials_path)
     updater = Updater(token=tg_bot_token, use_context=True)
     dispatcher = updater.dispatcher
 
     start_handler = CommandHandler('start', start)
     dispatcher.add_handler(start_handler)
 
-    echo_handler = MessageHandler(Filters.text & (~Filters.command), echo)
-    dispatcher.add_handler(echo_handler)
+    # echo_handler = MessageHandler(Filters.text & (~Filters.command), echo)
+    # dispatcher.add_handler(echo_handler)
+
+    answer_handler = MessageHandler(
+        Filters.text & (~Filters.command),
+        functools.partial(answer, project_id=project_id, language_code=language_code)
+    )
+    dispatcher.add_handler(answer_handler)
 
     updater.start_polling()
 
